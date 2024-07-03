af7ec07367258a41d8649abd134052a79a1f0987
CodeSandbox <support@codesandbox.io>
2024-07-03 12:11:52 +0000

    Sandbox snapshot commit

--- script.js
+++ script.js
@@ -35,8 +35,9 @@ clickables.forEach((elem, index) => {
 });
 
 const maxHeight = document.getElementsByTagName("body");
-let currentHeight = 500;
+let currentHeight = 550;
 const advTitle = document.querySelector(".adv__title");
+const aboutTitle = document.querySelector(".about__title");
 line.style.height = currentHeight + scrollY + "px";
 document.addEventListener("scroll", function () {
   const line = document.getElementById("line");
@@ -45,22 +46,35 @@ document.addEventListener("scroll", function () {
     scrollY = maxHeight;
   }
   line.style.height = currentHeight + scrollY + "px";
-  const rect = advTitle.getBoundingClientRect();
-  if (rect.bottom < 800 && rect.bottom > 0) {
-    const distanceToCircle = rect.top - window.innerHeight / 2; // Adjust window.innerHeight as needed
-
+  const advPos = advTitle.getBoundingClientRect();
+  if (advPos.bottom < 1000 && advPos.bottom > -200) {
+    const distanceToCircle = advPos.top - window.innerHeight / 2;
     if (distanceToCircle > 0) {
-      // Delay showing circle based on the distance
       setTimeout(function () {
         circle.style.display = "block";
         circle.style.animationPlayState = "running";
-      }, distanceToCircle + 1000);
+      }, distanceToCircle);
     } else {
-      circle.style.display = "block"; // Show immediately if already past the threshold
+      circle.style.display = "block";
       circle.style.animationPlayState = "running";
     }
   } else {
     circle.style.display = "none";
+  }
+  const aboutPos = aboutTitle.getBoundingClientRect();
+  if (aboutPos.bottom < 800 && aboutPos.bottom > 0) {
+    const distanceToCircle2 = aboutPos.top - window.innerHeight / 2;
+    if (distanceToCircle2 > 0) {
+      setTimeout(function () {
+        circle2.style.display = "block";
+        circle2.style.animationPlayState = "running";
+      }, distanceToCircle2);
+    } else {
+      circle2.style.display = "block";
+      circle2.style.animationPlayState = "running";
+    }
+  } else {
+    circle2.style.display = "none";
   }
 });
 
